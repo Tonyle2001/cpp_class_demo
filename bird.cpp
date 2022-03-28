7a5ff902ec02aaf71ac75d7850b94e2179196b88
Tonyle2001 <Tonyle200185@yahoo.com>
2022-03-28 00:01:51 -0500

    Create bird.cpp, bird.h, main.cpp and cMakeLists.txt files

--- bird.cpp
+++ bird.cpp
@@ -5,3 +5,13 @@
 //bird Class
 
 #include "bird.h"
+using namespace std;
+#include <iostream>
+
+    bird::bird(string species)
+    : bird_type{species}
+    {}
+
+    int bird::get_seeds(){
+        return this->seeds;
+    }
